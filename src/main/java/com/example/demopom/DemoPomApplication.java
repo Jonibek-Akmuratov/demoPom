2c47d2853840c1be774b311c023f84da6df5487c
jonibek <akmuratovj@gmail.com>
2022-08-17 19:23:09 +0500

    Initial commit

---
+++ src/main/java/com/example/demopom/DemoPomApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demopom/DemoPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
